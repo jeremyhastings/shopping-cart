682d6353e368a5485675732071c8105a14684daa
Jeremy Hastings <jeremy.hastings@mac.com>
2021-10-06 12:52:35 -0500

    Added Main and Checkout files

---
+++ checkout.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+class Checkout
+  attr_reader :price_rules
+
+  def initialize(price_rules)
+    @price_rules = price_rules
+    set_default_purchase_quantity
+  end
+
+  def scan(item)
+    purchases[item] += 1
+  end
+
+  def total
+    total_costs = 0
+    purchases.each do |item, quantity|
+      total_costs += if !discount_quantity(item) || quantity < discount_quantity(item)
+                       (item_price(item) * quantity)
+                     else
+                       (discount_price(item) * quantity)
+                     end
+    end
+    total_costs > total_basket_discount_level ? total_costs - (total_costs * basket_discount).to_i : total_costs
+  end
+
+  private
+
+  def purchases
+    @purchases ||= {}
+  end
+
+  def set_default_purchase_quantity
+    purchases.default = 0
+  end
+
+  def discount_quantity(item)
+    price_rules[item][:discount_quantity]
+  end
+
+  def item_price(item)
+    price_rules[item][:price]
+  end
+
+  def discount_price(item)
+    price_rules[item][:discount_price]
+  end
+
+  def total_basket_discount_level
+    price_rules[:discount][:total_basket]
+  end
+
+  def basket_discount
+    price_rules[:discount][:off]
+  end
+end
---
+++ main.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative 'checkout'
+
+PRICE_RULES = {
+  A: { price: 50, discount_quantity: 2, discount_price: 45 },
+  B: { price: 30, discount_quantity: 3, discount_price: 25 },
+  C: { price: 20, discount_quantity: nil, discount_price: nil },
+  discount: { total_basket: 200, off: 0.1 }
+}.freeze
+
+checkout = Checkout.new(PRICE_RULES)
+# checkout.scan(:A)
+# checkout.scan(:B)
+# checkout.scan(:C)
+
+# checkout.scan(:B)
+# checkout.scan(:A)
+# checkout.scan(:B)
+# checkout.scan(:B)
+# checkout.scan(:A)
+
+checkout.scan(:C)
+checkout.scan(:B)
+checkout.scan(:A)
+checkout.scan(:A)
+checkout.scan(:C)
+checkout.scan(:B)
+checkout.scan(:C)
+
+price = checkout.total
+
+puts price
