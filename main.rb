25bd2d2017088812808f84ac0d4eb52f8ec2175b
Jeremy Hastings <jeremy.hastings@mac.com>
2021-10-10 04:36:42 -0500

    Main as Object Oriented

--- main.rb
+++ main.rb
@@ -1,33 +1,101 @@
 # frozen_string_literal: true
 
-require_relative 'checkout'
+Item = Struct.new(:name, :price)
 
-PRICE_RULES = {
-  A: { price: 50, discount_quantity: 2, discount_price: 45 },
-  B: { price: 30, discount_quantity: 3, discount_price: 25 },
-  C: { price: 20 },
-  discount: { total_basket: 200, off: 0.1 }
-}.freeze
+QuantityDiscount = Struct.new(:name, :quantity, :discount) do
+  def qualify(cart, _sub_total)
+    discount_amount = 0
+    items = cart.purchases.select { |item| item.name == name }
+    discount_amount += (items.count * discount) if items.count >= quantity
+    discount_amount
+  end
+end
 
-# checkout = Checkout.new(PRICE_RULES)
-# checkout.scan(:A)
-# checkout.scan(:B)
-# checkout.scan(:C)
+BasketDiscount = Struct.new(:amount, :off) do
+  def qualify(_cart, sub_total)
+    discount_amount = 0
+    sub_total = sub_total.reduce(:+)
+    discount_amount -= (sub_total * off).to_i if sub_total >= amount
+    discount_amount
+  end
+end
 
-# checkout.scan(:B)
-# checkout.scan(:A)
-# checkout.scan(:B)
-# checkout.scan(:B)
-# checkout.scan(:A)
+item1 = Item.new(:A, 50)
+item2 = Item.new(:B, 30)
+item3 = Item.new(:C, 20)
 
-# checkout.scan(:C)
-# checkout.scan(:B)
-# checkout.scan(:A)
-# checkout.scan(:A)
-# checkout.scan(:C)
-# checkout.scan(:B)
-# checkout.scan(:C)
+quantity_discount1 = QuantityDiscount.new(:A, 2, -5)
+quantity_discount2 = QuantityDiscount.new(:B, 3, -5)
+basket_discount = BasketDiscount.new(200, 0.1)
 
-# price = checkout.total
+# discounts = [quantity_discount1, quantity_discount2, basket_discount]
+discounts = [quantity_discount1, quantity_discount2, basket_discount]
 
-# puts price
+class Cart
+  attr_reader :purchases
+
+  def scan(item)
+    purchases << item
+  end
+
+  def purchases
+    @purchases ||= []
+  end
+end
+
+class Checkout
+  attr_reader :cart, :discounts
+
+  def initialize(cart, discounts)
+    @discounts = discounts
+    @cart = cart
+  end
+
+  def total
+    total_cart
+    apply_discounts
+    final_price
+  end
+
+  private
+
+  def apply_discounts
+    discounts.each { |discount| sub_total << discount.qualify(cart, sub_total) }
+  end
+
+  def total_cart
+    cart.purchases.each { |item| sub_total << item.price }
+  end
+
+  def sub_total
+    @sub_total ||= []
+  end
+
+  def final_price
+    sub_total.reduce(:+)
+  end
+end
+
+cart = Cart.new
+# cart.scan(item1)
+# cart.scan(item2)
+# cart.scan(item3)
+
+# cart.scan(item2)
+# cart.scan(item1)
+# cart.scan(item2)
+# cart.scan(item2)
+# cart.scan(item1)
+
+cart.scan(item3)
+cart.scan(item2)
+cart.scan(item1)
+cart.scan(item1)
+cart.scan(item3)
+cart.scan(item2)
+cart.scan(item3)
+
+checkout = Checkout.new(cart, discounts)
+price = checkout.total
+
+puts price
